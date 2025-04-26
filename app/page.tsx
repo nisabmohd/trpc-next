5e29acef6e05abc23c3b6b14ec448cb9542a1c90
Mohd. Nisab <nisabmohd@gmail.com>
2025-04-26 21:33:25 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-export const dynamic = "force-dynamic";
-
 import { HydrateClient, trpc } from "@/trpc/server";
 import AddTodos from "../components/add-todos";
 import Todos from "../components/todos";
 import { Suspense } from "react";
+
+export const dynamic = "force-dynamic";
+
 
 export default async function Home() {
   void trpc.todos.getTodos.prefetch();
